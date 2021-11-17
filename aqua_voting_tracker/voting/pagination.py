7445f0bfdfad71278ad55cc13c72e662509869c2
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-11-17 20:36:12 +0300

    Add count to pagination.

--- aqua_voting_tracker/voting/pagination.py
+++ aqua_voting_tracker/voting/pagination.py
@@ -8,6 +8,27 @@ class VotingSnapshotCursorPagination(CursorPagination):
     page_size_query_param = 'limit'
     max_page_size = 200
 
+    def get_count(self, queryset):
+        """
+        Determine an object count, supporting either querysets or regular lists.
+        """
+        try:
+            return queryset.count()
+        except (AttributeError, TypeError):
+            return len(queryset)
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.count = self.get_count(queryset)
+        return super(VotingSnapshotCursorPagination, self).paginate_queryset(queryset, request, view=view)
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
+
 
 class FakePagination(BasePagination):
     def paginate_queryset(self, queryset, request, view=None):
@@ -15,6 +36,7 @@ class FakePagination(BasePagination):
 
     def get_paginated_response(self, data):
         return Response({
+            'count': len(data),
             'next': None,
             'previous': None,
             'results': data,
