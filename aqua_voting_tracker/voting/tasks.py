41052d829cde8faed7ce4baa728ac2d2a26b2059
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-12-08 14:28:00 +0300

    Limit queryset for "update claim back time" task.

--- aqua_voting_tracker/voting/tasks.py
+++ aqua_voting_tracker/voting/tasks.py
@@ -25,9 +25,13 @@ logger = logging.getLogger()
 
 
 CLAIMABLE_BALANCES_CURSOR_CACHE_KEY = 'aqua_voting_tracker.voting.CLAIMABLE_BALANCES_CURSOR_CACHE_KEY'
-CLAIMABLE_BALANCES_CACHE_TIMEOUT = None  # Forever
 CLAIMABLE_BALANCES_LIMIT = 200
 CLAIMABLE_BALANCES_BULK_LIMIT = 10000
+
+CLAIM_BACK_CURSOR_CACHE_KEY = 'aqua_voting_tracker.voting.CLAIM_BACK_CURSOR_CACHE_KEY'
+CLAIM_BACK_BUNCH_LIMIT = 100
+CLAIM_BACK_REQUEST_TIMEOUT = 60
+CLAIM_BACK_SEMAPHORE = 10
 
 
 def _parse_vote(claimable_balance: dict):
@@ -62,7 +66,7 @@ def task_load_new_claimable_balances():
     if last_claimable_balance:
         cache.set(CLAIMABLE_BALANCES_CURSOR_CACHE_KEY,
                   last_claimable_balance['paging_token'],
-                  CLAIMABLE_BALANCES_CACHE_TIMEOUT)
+                  None)
 
     Vote.objects.bulk_create(new_votes)
 
@@ -80,8 +84,9 @@ async def _update_claim_back_time(vote: Vote, *, server: ServerAsync, semaphore:
 
 
 async def _bunch_update_claim_back_time(votes: Iterable[Vote]):
-    semaphore = Semaphore(10)
-    async with ServerAsync(settings.HORIZON_URL, client=AiohttpClient(request_timeout=60)) as server:
+    semaphore = Semaphore(CLAIM_BACK_SEMAPHORE)
+    client = AiohttpClient(request_timeout=CLAIM_BACK_REQUEST_TIMEOUT)
+    async with ServerAsync(settings.HORIZON_URL, client=client) as server:
         await asyncio.gather(*[_update_claim_back_time(vote, server=server, semaphore=semaphore)
                                for vote in votes])
 
@@ -89,8 +94,18 @@ async def _bunch_update_claim_back_time(votes: Iterable[Vote]):
 @celery_app.task(ignore_result=True)
 def task_update_claim_back_time():
     now = timezone.now()
-    votes = list(Vote.objects.filter(locked_until__lt=now, claimed_back_at__isnull=True))
+    queryset = Vote.objects.filter(locked_until__lt=now, claimed_back_at__isnull=True)
+    cursor = cache.get(CLAIM_BACK_CURSOR_CACHE_KEY)
+    if cursor:
+        queryset = queryset.filter(id__gt=cursor)
+
+    votes = list(queryset[:CLAIM_BACK_BUNCH_LIMIT])
     asyncio.run(_bunch_update_claim_back_time(votes))
+
+    if len(votes) < CLAIM_BACK_BUNCH_LIMIT:
+        cache.delete(CLAIM_BACK_CURSOR_CACHE_KEY)
+    else:
+        cache.set(CLAIM_BACK_BUNCH_LIMIT, votes[-1].id, None)
 
 
 @celery_app.task(ignore_result=True)
