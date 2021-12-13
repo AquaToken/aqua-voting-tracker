86fe5dc9b070fc8ea09b1f5133ca008684f32aa6
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-12-13 16:07:26 +0300

    Fix data getters for voting rewards calculation.

--- aqua_voting_tracker/voting_rewards/data.py
+++ aqua_voting_tracker/voting_rewards/data.py
@@ -1,6 +1,7 @@
 from typing import Iterable, Mapping
 
 import requests
+from django.conf import settings
 
 from aqua_voting_tracker.voting.models import VotingSnapshot
 from aqua_voting_tracker.voting.serializers import VotingSnapshotSerializer, VotingSnapshotStatsSerializer
@@ -8,15 +9,16 @@ from aqua_voting_tracker.voting.serializers import VotingSnapshotSerializer, Vot
 
 def get_voting_rewards_candidate() -> Iterable[Mapping]:
     queryset = VotingSnapshot.objects.filter_last_snapshot().order_by('-votes_value')[:100]
-    return VotingSnapshotSerializer(instance=queryset, many=True).data()
+    return VotingSnapshotSerializer(instance=queryset, many=True).data
 
 
 def get_voting_stats() -> Mapping:
     stats = VotingSnapshot.objects.current_stats()
-    return VotingSnapshotStatsSerializer(instance=stats).data()
+    return VotingSnapshotStatsSerializer(instance=stats).data
 
 
 def get_market_pairs(market_keys: Iterable[str]) -> Iterable[Mapping]:
-    resp = requests.get('https://marketkeys-tracker.aqua.network/api/market-keys/',
+    market_keys_tracker_url = settings.MARKETKEYS_TRACKER_URL.rstrip('/')
+    resp = requests.get(f'{market_keys_tracker_url}/api/market-keys/',
                         params=[('account_id', market_key) for market_key in market_keys])
     return resp.json()['results']
